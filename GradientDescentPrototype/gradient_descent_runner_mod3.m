73fc995813ac31e57d501c0f979c631d142f5aac
ShimonaNiharika <Shimona.niharika.Dsouza@gmail.com>
2017-10-13 16:57:36 -0400

    3 rd model + normalisation

---
+++ GradientDescentPrototype/gradient_descent_runner_mod3.m
@@ -0,0 +1,40 @@
+function [B,C,C_hat,W] = gradient_descent_runner_mod3(corr,B_init,C_init,C_hat_init,W_init,Y,lambda,lambda_1,lambda_2,lambda_3,lambda_4,lr1)
+%%runs gradient descent using alternating minimisation
+
+%Initilise
+num_iter =1000;
+B_old = B_init;
+C_old = C_init;
+C_hat_old=C_hat_init;
+W_old = W_init;
+thresh = 10e-06;
+
+%Iterate
+err =[];
+% plot(0,0)
+% title('Graident Descent Run ')
+% xlabel('Number of iterations')
+% ylabel('Value of obejctive function')
+
+for i = 1:num_iter
+    err= horzcat(err,error_compute_mod3(corr,B_old,C_old,C_hat_old,Y,W_old,lambda,lambda_1,lambda_2,lambda_3,lambda_4));
+    fprintf(' At iteration %d || Error: %f \n',i,err(i))
+%     plot(1:i,err,'b');
+%     hold on;
+%     drawnow;
+    if (i<10)
+        [B,C,C_hat,W] = step_gradient(corr,B_old,C_old,C_hat_old,W_old,Y,lambda,lambda_1,lambda_2,lambda_3,lambda_4,0.0001); 
+    else 
+        [B,C,C_hat,W] = step_gradient(corr,B_old,C_old,C_hat_old,W_old,Y,lambda,lambda_1,lambda_2,lambda_3,lambda_4,lr1); 
+    
+    end
+    B_old = B;
+    C_hat_old =C_hat;
+    C_old = C;
+    W_old =W;
+    if (i>1 && abs(err(i)-err(i-1)) < thresh)
+        break;
+    end
+end
+
+end
--- GradientDescentPrototype/step_gradient_mod3.m
+++ GradientDescentPrototype/step_gradient_mod3.m
@@ -1,4 +1,4 @@
-function [B_upd,C_upd,W_upd] = step_gradient_old(corr,B,C,W,Y,lambda,lambda_1,lambda_2,lambda_3,lambda_4,lr1)
+function [B_upd,C_upd,C_hat_upd,W_upd] = step_gradient_mod3(corr,B,C,C_hat,W,Y,lambda,lambda_1,lambda_2,lambda_3,lambda_4,lr1)
 %%Given the current values of the iterates, performs a single step of
 %%gradient descent using alternating minimisation
 num_iter_max =100;
@@ -6,11 +6,11 @@ num_iter_max =100;
 fprintf('Optimise B')
 err =[];
 % plot(0,0)
-
+B_hat = eye(size(corr,2));
 for i = 1:num_iter_max
     
     grad_B = zeros(size(B));
-    err= horzcat(err,error_compute_old(corr,B,C,Y,W,lambda,lambda_1,lambda_2,lambda_3,lambda_4));
+    err= horzcat(err,error_compute(corr,B,C,C_hat,Y,W,lambda,lambda_1,lambda_2,lambda_3,lambda_4));
     fprintf('\n B update iteration %d || error: %f ',i,err(i));
     
 %     plot(1:i,err,'r')
@@ -21,8 +21,8 @@ for i = 1:num_iter_max
     for n = 1:size(corr,1)
         A_n = diag(C(:,n));
         Corr_mat = reshape(corr(n,:,:),[size(corr,2),size(corr,3)]);
-        %B_hat_mat = reshape(B_hat(n,:,:),[size(corr,2),size(corr,3)]);
-        grad_B = grad_B - 4* (Corr_mat)*B*A_n + 4*B*A_n*(B'*B)*A_n;
+        B_hat = eye(size(corr,2));
+        grad_B = grad_B - 4* (Corr_mat-B_hat*diag(C_hat(:,n))*B_hat')*B*A_n + 4*B*A_n*(B'*B)*A_n;
     end
 
     grad_B = grad_B -lambda_4*4*B+lambda_4*4*(B*B')*B;
@@ -34,23 +34,16 @@ for i = 1:num_iter_max
 
     B_upd = B - lr1*(grad_B + lambda_1* signum_B);
     lr1 = lr1*0.1;
-    
+    B_upd = normc(B_upd);
     if ((i>1) && (abs(err(i)-err(i-1))< 10e-06 || err(i-1)<= err(i)))      
-        B_upd = normc(B_upd);
         break;
     end
+    
     if ((i>1)&&err(i)<= err(i-1))
-        B_upd = normc(B_upd);
         B = B_upd;
     end
     
 end
-
-
-%% B_hat update
-
-%B_hat_upd = quad_estimate_B_hat(B_upd,C,corr);
-
 
 %% C update
 
@@ -60,8 +53,7 @@ for m = 1:n
    
     H = 2*((B_upd'*B_upd).^2 + lambda*(W*W')+ lambda_2* eye(size(B_upd'*B_upd)));
     Corr_mat = reshape(corr(m,:,:),[size(corr,2),size(corr,3)]);
-    %B_hat_mat = reshape(B_hat(m,:,:),[size(corr,2),size(corr,3)]);
-    M = -2*(B_upd'*(Corr_mat)*B_upd);
+    M = -2*(B_upd'*(Corr_mat-B_hat*diag(C_hat(:,m))*B_hat')*B_upd);
     f = diag(M) -2*lambda*Y(m)*W;
     A = -eye(size(C,1));
     b = zeros(size(C,1),1);
@@ -69,6 +61,20 @@ for m = 1:n
     C_upd(:,m) = c_m;
 end
 
+%% C_hat update
+
+C_hat_upd = zeros(size(C_hat));
+for m = 1:n
+   
+    H = 2*B_hat;
+    Corr_mat = reshape(corr(m,:,:),[size(corr,2),size(corr,3)]);
+    M = -2*(Corr_mat-B_upd*diag(C_upd(:,m))*B_upd');
+    f = diag(M);
+    A = -eye(size(C_hat,1));
+    b = zeros(size(C_hat,1),1);
+    c_m = quadprog(H,f,A,b);
+    C_hat_upd(:,m) = c_m;
+end
 
 %% W update
 % epsil = 10e-06;
